f9dd1a8eebf51367403cbd7fd4582107a94ba3ff
Amit Sahu <sahuamit4783@gmail.com>
2026-06-25 15:21:02 +0530

    day2- chaning recusivechunking with limited length

--- backend/app/chunking/chunk_service.py
+++ backend/app/chunking/chunk_service.py
@@ -2,15 +2,37 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 text_splitter = RecursiveCharacterTextSplitter(
+
     chunk_size=800,
+
     chunk_overlap=150,
+
+    length_function=len,
+
+    is_separator_regex=False,
+
     separators=[
+
         "\n\n",
+
         "\n",
+
         ". ",
+
+        "? ",
+
+        "! ",
+
+        "; ",
+
+        ", ",
+
         " ",
+
         ""
+
     ]
+
 )
 
 
