f09f6bd156fa8d0b4d19e5eac9cd8e620e733f52
Amit Sahu <sahuamit4783@gmail.com>
2026-06-27 14:17:37 +0530

    day4- created

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-from app.routes.upload import router
+
+from app.routes.upload import router as upload_router
+from app.routes.search import router as search_router
 
 app = FastAPI(
     title="ScholarSync MVP"
 )
 
-app.include_router(router)
+app.include_router(upload_router)
+app.include_router(search_router)
