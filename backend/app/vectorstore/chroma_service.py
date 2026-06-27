85ea114602747791824e1798e60c7cae689e67a7
Amit Sahu <sahuamit4783@gmail.com>
2026-06-27 14:39:27 +0530

    day4- add search chunks function

--- backend/app/vectorstore/chroma_service.py
+++ backend/app/vectorstore/chroma_service.py
@@ -52,3 +52,15 @@ def search(query_embedding, top_k=5):
     )
 
     return results    
+
+def search_chunks(query_embedding, top_k=5):
+    """
+    Search the most similar chunks from ChromaDB.
+    """
+
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=top_k
+    )
+
+    return results
