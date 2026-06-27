8d0defc74e9e8578c74f67a531ef518676adfd93
Amit Sahu <sahuamit4783@gmail.com>
2026-06-27 14:38:52 +0530

    day4- add search embedding function

--- backend/app/embeddings/embedding_service.py
+++ backend/app/embeddings/embedding_service.py
@@ -8,3 +8,10 @@ def generate_embeddings(texts):
     Generate vector embeddings for a list of text chunks.
     """
     return model.encode(texts).tolist()
+
+def generate_query_embedding(query):
+    """
+    Generate embedding for a user's search query.
+    """
+    embedding = model.encode(query)
+    return embedding.tolist()
