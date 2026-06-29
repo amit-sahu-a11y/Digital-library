379eeb65ffb72839fe3b615ed0b0cdaf819584e2
Amit Sahu <sahuamit4783@gmail.com>
2026-06-29 07:19:34 +0530

    day5- create gemini api and setup completed

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI
 from app.routes.upload import router as upload_router
 from app.routes.search import router as search_router
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
 app = FastAPI(
     title="ScholarSync MVP"
 )
