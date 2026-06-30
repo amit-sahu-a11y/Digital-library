77d1a844f53b76d33169465b3bd2b89b910ff639
Amit Sahu <sahuamit4783@gmail.com>
2026-06-30 11:07:00 +0530

    creating topbar.py

---
+++ frontend/src/components/TopBar.jsx
@@ -0,0 +1,22 @@
+const TopBar = () => {
+  return (
+    <header className="topbar">
+
+      <div className="logo">
+        ScholarSync AI
+      </div>
+
+      <div className="user">
+
+        <img
+          src="https://i.pravatar.cc/100"
+          alt="user"
+        />
+
+      </div>
+
+    </header>
+  );
+};
+
+export default TopBar;
